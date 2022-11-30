77a8ecabdc1c0e44df64630a3071277e973a5d49
guopenghui <guopeng_hui@126.com>
2022-11-30 20:28:41 +0800

    add zh-TW

--- src/lang/helper.ts
+++ src/lang/helper.ts
@@ -1,15 +1,17 @@
-import zh from "./locale/zh"
-import en from "./locale/en"
+import zh from "./locale/zh";
+import en from "./locale/en";
+import zhTW from "./locale/zh-TW";
 
-const localeMap: { [k: string]: Partial<typeof en> } = {
+const localeMap: { [k: string]: Partial<typeof en>; } = {
     en,
     zh,
-}
+    "zh-TW": zhTW,
+};
 
 
-const lang = window.localStorage.getItem("language")
-const locale = localeMap[lang || "en"]
+const lang = window.localStorage.getItem("language");
+const locale = localeMap[lang || "en"];
 
 export function t(text: keyof typeof en): string {
-    return (locale && locale[text]) || en[text]
+    return (locale && locale[text]) || en[text];
 }
