203a26b2f964df9c50956b4418e9b0bf4cf39185
guopenghui <guopeng_hui@126.com>
2022-12-21 13:29:23 +0800

    update manifest

--- publish.mjs
+++ publish.mjs
@@ -8,8 +8,12 @@ const root = process.cwd();
 
 // write version
 let manifest = JSON.parse(fs.readFileSync(path.join(root, "manifest.json"), "utf8"));
-manifest.version = version;
-fs.writeFileSync(path.join(root, "manifest.json"), JSON.stringify(manifest));
+if (manifest.version !== version) {
+    manifest.version = version;
+    fs.writeFileSync(path.join(root, "manifest.json"), JSON.stringify(manifest, null, 4));
+    // message must use " instead of ' on windows
+    child.execSync('git commit -am "update manifest"');
+}
 
 child.execSync("git push");
 child.execSync(`git tag ${version}`);
