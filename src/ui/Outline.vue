a7ddc6ee373d7158b909cf8c30f9563ec3e18ccb
guopenghui <guopeng_hui@126.com>
2025-04-26 23:33:34 +0800

    feat: add drag ghost

--- src/ui/Outline.vue
+++ src/ui/Outline.vue
@@ -769,6 +769,36 @@ function reset() {
     switchLevel(level.value);
 }
 
+
+onMounted(() => {
+	container.addEventListener("dragstart", (e) => {
+		if(!plugin.navigator.canDrop) {
+			return;
+		}
+		
+		const target = e.target as HTMLElement;
+		if(!target || !target.hasClass("n-tree-node")) {
+			return;
+		}
+		
+		const no = parseInt(target.id.slice(3));
+		const heading = store.headers[no];
+		
+		e.dataTransfer?.setData("text/plain", heading.heading)
+		// @ts-ignore
+		plugin.app.dragManager.onDragStart(e, {
+			source: "outline",
+			type: "heading",
+			icon: "heading-glyph",
+			title: heading.heading,
+			heading,
+			// @ts-ignore 
+			// currently only markdownNav allows drop, and its view exists
+			file: plugin.navigator.view.file
+		});
+	})
+});
+
 // drag and drop
 async function onDrop({ node, dragNode, dropPosition }: TreeDropInfo) {
     if (!plugin.navigator.canDrop) {
