5d25debe217f4a9e069b6392ac3cd006d57015d3
guopenghui <guopeng_hui@126.com>
2026-07-09 18:02:21 +0800

    fix: update located heading styles

--- src/ui/Outline.vue
+++ src/ui/Outline.vue
@@ -242,8 +242,16 @@ defineExpose({
 }
 
 /* located heading*/
-.n-tree-node.located p {
-    color: v-bind(primaryColor);
+
+.quiet-outline .n-tree {
+    color: var(--nav-item-color);
+}
+
+.quiet-outline .n-tree-node-wrapper {
+    border-radius: var(--nav-item-radius);
+    &:has(.n-tree-node.located) {
+        background-color: var(--nav-item-background-active);
+    }
 }
 
 /* adjust indent */
